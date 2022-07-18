5b36c2001a7a67103258b9bc730155c9943ebeef
Jeff <jeffreyjohn102@gmail.com>
2022-07-18 19:52:14 -0400

    Add option for random stroke color

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const canvas = document.querySelector('.main-canvas');
 let color = document.querySelector('#color');
+const randomColorBtn = document.querySelector('#randomColor');
 
-isClicked = false;
+let isClicked = false;
+let isRandomColor = false;
 
-console.dir(document.querySelector('input'));
+// console.dir(document.querySelector('input'));
 
 for (let i = 1; i <= 256; i++) {
     let pixel = document.createElement('div');
@@ -11,16 +13,30 @@ for (let i = 1; i <= 256; i++) {
     canvas.appendChild(pixel);
 }
 
+randomColorBtn.addEventListener('click', function () {
+    isRandomColor = true;
+})
+
 canvas.addEventListener('mousedown', function (e) {
     isClicked = true;
 })
-
 canvas.addEventListener('mousemove', function (e) {
     if (isClicked) {
-        e.target.style.background = color.value;
+        if (isRandomColor) {
+            e.target.style.background = randomColor();
+
+        } else e.target.style.background = color.value;
     }
 })
-
-canvas.addEventListener('mouseup', function(e) {
+canvas.addEventListener('mouseup', function (e) {
     isClicked = false;
+    isRandomColor = false;
 })
+
+function randomColor() {
+    R = Math.floor(Math.random() * 256);
+    G = Math.floor(Math.random() * 256);
+    B = Math.floor(Math.random() * 256);
+    
+    return `rgb(${R}, ${G}, ${B})`;
+}
