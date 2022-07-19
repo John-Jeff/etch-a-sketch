d4d37adf567484b06bd4e3ad0309cbb7dc300066
Jeff <jeffreyjohn102@gmail.com>
2022-07-18 22:03:24 -0400

    Add grid size selection slider

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const canvas = document.querySelector('.main-canvas');
 let color = document.querySelector('#color');
-const randomColorBtn = document.querySelector('#randomColor');
+const randomColorBtn = document.querySelector('#random-color');
+const gridSize = document.querySelector('#grid-size');
+const gridRows = document.querySelector('#grid-rows');
+const gridColumns = document.querySelector('#grid-columns');
 
 let isClicked = false;
 let isRandomColor = false;
 
 // console.dir(document.querySelector('input'));
 
-for (let i = 1; i <= 256; i++) {
+for (let i = 1; i <= (16 * 16); i++) {
     let pixel = document.createElement('div');
     pixel.classList.add('pixel');
     canvas.appendChild(pixel);
 }
 
-randomColorBtn.addEventListener('click', function () {
+gridSize.addEventListener('input', function () {
+    gridRows.textContent = gridSize.value;
+    gridColumns.textContent = gridSize.value;
+})
+
+gridSize.addEventListener('mouseup', function (e) {
+    setGridSize(gridSize.value);
+})
+
+randomColorBtn.addEventListener('click', function (e) {
     isRandomColor = true;
 })
 
@@ -38,10 +50,24 @@ canvas.addEventListener('mouseup', function (e) {
     isClicked = false;
 })
 
+function setGridSize(size) {
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
+    }
+    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 1; i <= (size * size); i++) {
+        let pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        canvas.appendChild(pixel);
+    }
+}
+
 function randomColor() {
     R = Math.floor(Math.random() * 256);
     G = Math.floor(Math.random() * 256);
     B = Math.floor(Math.random() * 256);
-    
+
     return `rgb(${R}, ${G}, ${B})`;
 }
